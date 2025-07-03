ccb842907ca5d4e244b9545fcb409dd203963f70
Documenter.jl <documenter@juliadocs.github.io>
2025-07-03 15:07:07 +0000

    build based on a134187

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CompositionalJulia","page":"Home","title":"CompositionalJulia","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CompositionalJulia.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
